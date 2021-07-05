c994ee1aff50b1abe4b963e99c10e82a322096ef
Connor Zanin <cnnrznn@gmail.com>
2021-07-05 17:21:34 -0600

    Prettier defered print

--- things/towers/hanoi.go
+++ things/towers/hanoi.go
@@ -12,26 +12,26 @@ type Hanoi struct {
 }
 
 func main() {
-	for n := 1; n < 100; n++ {
+	for n := 1; n < 30; n++ {
 		h := &Hanoi{}
 
 		for i := n; i > 0; i-- {
 			h.s[0].Push(i)
 		}
 
-		//h.Solve(0, 1, 2, n)
-		//fmt.Printf("%v solved in %v moves!\n", n, h.nmoves)
-
-		fmt.Println(n, CountHanoi(n))
+		h.Solve(0, 1, 2, n)
 	}
 }
 
 func (h *Hanoi) Solve(start, temp, target, n int) {
-	//fmt.Printf("Level %v\n", n)
-	//fmt.Println(h.s[0])
-	//fmt.Println(h.s[1])
-	//fmt.Println(h.s[2])
-	//fmt.Println()
+	defer func() {
+		fmt.Printf("Level %v\n", n)
+		fmt.Println(h.s[0])
+		fmt.Println(h.s[1])
+		fmt.Println(h.s[2])
+		fmt.Println()
+	}()
+
 	if n == 1 {
 		h.s[target].Push(h.s[start].Pop())
 		h.nmoves++
@@ -41,11 +41,6 @@ func (h *Hanoi) Solve(start, temp, target, n int) {
 	h.s[target].Push(h.s[start].Pop())
 	h.nmoves++
 	h.Solve(temp, start, target, n-1)
-	//fmt.Printf("Level %v\n", n)
-	//fmt.Println(h.s[0])
-	//fmt.Println(h.s[1])
-	//fmt.Println(h.s[2])
-	//fmt.Println()
 }
 
 func CountHanoi(n int) int {
